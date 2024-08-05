b3c4afbf1f43f6dbb726c49194788b4a74e8bd2f
Natalia Bandurova <natacsp@mail.ru>
2024-08-05 09:51:57 +0300

    fix: fix download

--- src/js/downloadPdf.js
+++ src/js/downloadPdf.js
@@ -2,6 +2,9 @@ import html2pdf from 'html2pdf.js';
 
 const downloadButton = document.querySelector('#download-button');
 downloadButton.addEventListener('click', () => {
+  document.querySelectorAll('.ripple').forEach((ripple) => {
+    ripple.style.display = 'none';
+  });
   const resume = document.getElementById('resume');
   const opt = {
     margin: 0.5,
@@ -10,5 +13,13 @@ downloadButton.addEventListener('click', () => {
     html2canvas: { scale: 2 },
     jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
   };
-  html2pdf().from(resume).set(opt).save();
+  html2pdf()
+    .from(resume)
+    .set(opt)
+    .save()
+    .then(() => {
+      document.querySelectorAll('.ripple').forEach((ripple) => {
+        ripple.style.display = '';
+      });
+    });
 });
